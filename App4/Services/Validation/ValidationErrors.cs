87cadf7b3614368d9e60fdaa5b67af04994f3e82
rev3ng <konsle17@wp.pl>
2018-08-21 10:51:26 +0200

    Add Validation Errors service base class

---
+++ App4/Services/Validation/ValidationErrors.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GalaSoft.MvvmLight;
+
+namespace App4.Services.Validation
+{
+	class ValidationErrors : ViewModelBase
+	{
+		private readonly Dictionary<string, string> _validationErrors = new Dictionary<string, string>();
+
+		public bool IsValid
+		{
+			get { return this._validationErrors.Count < 1; }
+		}
+
+		public string this[string fieldName]
+		{
+			get
+			{
+				return this._validationErrors.ContainsKey(fieldName) ? this._validationErrors[fieldName] : string.Empty;
+			}
+
+
+
+			set
+			{
+				if (this._validationErrors.ContainsKey(fieldName))
+				{
+					if (string.IsNullOrWhiteSpace(value))
+					{
+						this._validationErrors.Remove(fieldName);
+					}
+					else
+					{
+						this._validationErrors[fieldName] = value;
+					}
+				}
+				else
+				{
+					if (!string.IsNullOrWhiteSpace(value))
+					{
+						this._validationErrors.Add(fieldName, value);
+					}
+				}
+
+				this.RaisePropertyChanged();
+				this.RaisePropertyChanged("InValid");
+			}
+		}
+	}
+}
